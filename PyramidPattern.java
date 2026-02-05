e293b91f361402bad6750e0506985e89ca702d49
Taufeeq6t5 <taufeeqahmed.iet@srinivasuniversity.edu.in>
2026-02-05 17:46:01 +0530

    Updated Program

---
+++ PyramidPattern.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class PyramidPattern {
+    
+    public static void main (String args[]){
+
+        Scanner sc=new Scanner(System.in);
+
+        System.out.println("Enter the number rows: ");
+        int n=sc.nextInt();
+
+        for(i=1;i<=n ;i++){
+
+ 
+            
+            }
+            System.out.println();
+
+
+        }
+
+
+        
+
+    }
+}
