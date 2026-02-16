2fb99dcb47b829104d8cb1d9b8c20a1161df9fe9
Taufeeq Ahmed <taufeeqahmed.iet@srinivasuniversity.edu.in>
2026-02-16 16:17:19 +0530

    Update PyramidPattern.java

--- PyramidPattern.java
+++ PyramidPattern.java
@@ -1,9 +1,7 @@
 package PatternProgram;
-
 import java.util.Scanner;
 
 public class PyramidPattern {
-    
     public static void main (String args[]){
 
         Scanner sc=new Scanner(System.in);
@@ -15,14 +13,11 @@ public class PyramidPattern {
 
         for(int j=1;j<=n-i;j++){
         System.out.print(" ");
-        }
-        
+        }        
         for(int k=1;k<=2*i-1;k++){
         System.out.print("*");
-        }
-        
+        }  
         System.out.println();
-
     }
     sc.close();
         
