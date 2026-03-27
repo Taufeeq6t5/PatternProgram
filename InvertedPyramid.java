d4ebc567c329439c236dce44e73318c4cec869f4
Taufeeq Ahmed <taufeeqahmed.iet@srinivasuniversity.edu.in>
2026-03-27 20:50:52 +0530

    Fix formatting in InvertedPyramid.java

--- InvertedPyramid.java
+++ InvertedPyramid.java
@@ -10,6 +10,7 @@ public class InvertedPyramid{
         int n=sc.nextInt();
 
         for (int i=1;i<=n;i++){
+            
             for (int j=1;j<=i-1;j++){
                 System.out.print(" ");
             }
