3e2594ef5f25084b7d802c3a4024688461c82e8c
Taufeeq Ahmed <taufeeq6t5@gmail.com>
2026-04-04 14:32:55 +0530

    Implement hollow triangle pattern in HollowTriangle.java

--- HollowTriangle.java
+++ HollowTriangle.java
@@ -13,5 +13,18 @@ public class HollowPyramid {
             for (int j = 1; j <= n - i; j++) {
                 System.out.print(" ");
             }
-          
+          for (int j = 1; j <= (2 * i - 1); j++) {
+
+                if (j == 1 || j == (2 * i - 1) || i == n) {
+                    System.out.print("*");
+                } else {
+                    System.out.print(" ");
+                }
+            }
+            System.out.println();
+        }
+        sc.close();
+    }
+}
+
             
