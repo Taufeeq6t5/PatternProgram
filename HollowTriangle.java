00ba761414164cd2afbe9b702477ff0cee99da35
Taufeeq Ahmed <taufeeq6t5@gmail.com>
2026-04-04 14:32:27 +0530

    Add HollowPyramid class to print hollow pyramid

--- HollowTriangle.java
+++ HollowTriangle.java
@@ -1,1 +1,17 @@
+import java.util.Scanner;
 
+public class HollowPyramid {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter number of rows: ");
+        int n = sc.nextInt();
+
+        for (int i = 1; i <= n; i++) {
+
+            for (int j = 1; j <= n - i; j++) {
+                System.out.print(" ");
+            }
+          
+            
