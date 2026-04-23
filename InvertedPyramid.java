27fe6cdffef50796aa42704e506a457e2b7081e3
Taufeeq Ahmed <taufeeq6t5@gmail.com>
2026-04-23 13:44:28 +0530

    Fix indentation in InvertedPyramid.java

--- InvertedPyramid.java
+++ InvertedPyramid.java
@@ -10,7 +10,6 @@ public class InvertedPyramid{
         int n=sc.nextInt();
 
         for (int i=1;i<=n;i++){
-            
             for (int j=1;j<=i-1;j++){
                 System.out.print(" ");
             }
